8c34ac8326043358519954d248e38bc7def4e3cc
Nargalek <vaakomolinari@gmail.com>
2018-03-18 19:00:00 +0100

    Add files via upload

--- potegowanie_Gadecki.cpp
+++ potegowanie_Gadecki.cpp
@@ -9,37 +9,37 @@ Wykonal Jakub Gadecki.
 
 */
 
-#include <iostream>
+#include <iostream> //deklaracja input output stream do wyduku i odczytu liczb
 
 using namespace std;
 
 int main(){
-	double a,c=1;
-	int b,d;
+	double a,c=1; //deklaracja zmiennych a - podstawa, c - zmienna wynikowa
+	int b,d; //deklaracja zmiennych b - wykladnik, d - zmienna pomocnicza
 	
 	
-	cout<<"\nPodaj wartosc podstawy: ";
-	cin>>a;
-	cout<<"\nPodaj wykladnik: ";
-	cin>>b;
-	
-	if(b<0){
+	cout<<"\nPodaj wartosc podstawy: "; //wyswietlenie prosby o podanie podstawy
+	cin>>a;		//odczyt wartosci podstawy
+	cout<<"\nPodaj wykladnik: "; //wyswietlenie prosby o podanie wykladnika
+	cin>>b; //odczyt wartosci wykladnika
+	if(!cin)cout<<"\nTylko liczby moga zostac uzyte!\n"; //sprawdza czy zmienna moze zostac uzyta (musi byc liczba)
+	else if(a==0)cout<<"\nWynik: 0\n"; //sprawdza czy podstawa jest zerem
+	else if(a<0 && b==0)cout<<"\nWynik: -1\n"; //sprawdza ujemna podstawa czy ma wykladnik 0
+	else{
+	if(b<0){		//wyrazenie dajace wartosc bezwzgledna ujemnego wykladnika
 		b=-b;
 		d=1;
 	}
-	
-	while(b-->0){
+	while(b-->0){	//petla potegujaca
 		c*=a;
-
 	}
-	
-	while(d--!=0) {
+	while(d--!=0) {	//petla obliczajaca wynik jesli wykladnik byl ujemny
 		c=1/c;
 	}
+
 	
-	
-	cout<<"\nWynik: "<<c<<"\n\n";
-	
-	system("PAUSE");
+	cout<<"\nWynik: "<<c<<"\n\n"; //wstrzumujemy program i czekamy na reakcje
+	}
+	system("PAUSE");//czynimy zadosc funkcji main()
 	return 0;
 }
